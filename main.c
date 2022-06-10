76b7088c6369286cda1cc8a894a08af68c6c3df7
Cebrayil Balayev <cebrayilbalayev@gmail.com>
2022-06-10 17:15:19 +0400

    Step 1

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <unistd.h>
 
-#define size 5
+#define size 32
 
 int **allocatedMatrix(){
 	int **matrix;
@@ -14,7 +14,17 @@ int **allocatedMatrix(){
 }
 
 int random0n(int n){
-	return random()%n;// 0 to n not included
+	return random()%n;
+}
+
+int **randomMatrix(int **mat){
+	int **matrix = mat;
+
+	for(int i=0;i<size;i++)
+		for(int j=0;j<size;j++)
+			matrix[i][j]=random0n(2);
+		
+	return matrix;
 }
 
 int activate(){
@@ -33,7 +43,7 @@ int isalive(int b){
 void showMatrix(int **array){
 	for(int i=0;i<size;i++){
 		for(int j=0;j<size;j++)
-			if(isalive(array[i][j])) printf("\033[47m  \033[0m");
+			if(isalive(array[i][j])) printf("\033[43m  \033[0m");
 			//if(isalive(array[i][j])) printf("\033[4%dm  \033[0m",random0n(7)+1);
 			else  printf("\033[40m  \033[0m");
 	printf("\n");}printf("\n");
@@ -47,20 +57,36 @@ void showsimpleMatrix(int **array){
 }
 
 
-int numberOfneighbors(int **array,int row,int column){
-	int n=0;
+int numberOfNeighborsClipped(int **array,int row,int column){
+	int nbNeigh=0;
 
-	if(row) if(isalive(array[row-1][column])) n++;
-	if(column) if(isalive(array[row][column-1])) n++;
-	if(row < size-1) if(isalive(array[row+1][column])) n++;
-	if(column < size-1) if(isalive(array[row][column+1])) n++;
+	if(row) if(isalive(array[row-1][column])) nbNeigh++;
+	if(column) if(isalive(array[row][column-1])) nbNeigh++;
+	if(row < size-1) if(isalive(array[row+1][column])) nbNeigh++;
+	if(column < size-1) if(isalive(array[row][column+1])) nbNeigh++;
 
-	if(row && column) if(isalive(array[row-1][column-1])) n++;
-	if(row && column < size-1) if(isalive(array[row-1][column+1])) n++;
-	if(row < size-1 && column) if(isalive(array[row+1][column-1])) n++;
-	if(row < size-1 && column < size-1) if(isalive(array[row+1][column+1])) n++;
+	if(row && column) if(isalive(array[row-1][column-1])) nbNeigh++;
+	if(row && column < size-1) if(isalive(array[row-1][column+1])) nbNeigh++;
+	if(row < size-1 && column) if(isalive(array[row+1][column-1])) nbNeigh++;
+	if(row < size-1 && column < size-1) if(isalive(array[row+1][column+1])) nbNeigh++;
 
-	return n;
+	return nbNeigh;
+}
+
+int numberOfNeighborsCircular(int **array,int row,int column){
+	int nbNeigh=0;
+
+	if(isalive(array[(row+size-1)%size][column])) nbNeigh++;
+	if(isalive(array[row][(column+size-1)%size])) nbNeigh++;
+	if(isalive(array[(row+size+1)%size][column])) nbNeigh++;
+	if(isalive(array[row][(column+size+1)%size])) nbNeigh++;
+
+	if(isalive(array[(row+size-1)%size][(column+size-1)%size])) nbNeigh++;
+	if(isalive(array[(row+size-1)%size][(column+size+1)%size])) nbNeigh++;
+	if(isalive(array[(row+size+1)%size][(column+size-1)%size])) nbNeigh++;
+	if(isalive(array[(row+size+1)%size][(column+size+1)%size])) nbNeigh++;
+
+	return nbNeigh;
 }
 
 int **matrixOfNeighbors(int **array){
@@ -68,7 +94,7 @@ int **matrixOfNeighbors(int **array){
 
 	for(int i=0;i<size;i++)
 		for(int j=0;j<size;j++)
-			neighbors[i][j] = numberOfneighbors(array,i,j);
+			neighbors[i][j] = numberOfNeighborsCircular(array,i,j);//numberOfNeighborsClipped(array,i,j);
 
 	return neighbors;
 }
@@ -91,44 +117,17 @@ int **dostep(int **array){
 	return returnArray;
 }
 
-void writeTo(int **array,FILE *pFile){
-	for(int i=0;i<size;i++){
-		for(int j=0;j<size;j++)
-			if(array[i][j])fprintf(pFile,"1" );
-			else fprintf(pFile,"0" );
-		//fprintf(pFile,"\n" );
-	}//fprintf(pFile,"\n" );
-}
-
 int main(){
-	srand(time(NULL)); // randomize the seed
+	srand(time(NULL));
 	int **array = allocatedMatrix();
-
-	for(int i=0;i<size;i++)
-		for(int j=0;j<size;j++)
-			array[i][j]=random0n(2);
+	array = randomMatrix(array);
 
 	showMatrix(array);
 
-	FILE *pFile;
-
-	pFile=fopen("module", "w");
-	if(pFile==NULL) perror("Error opening file.");
-
-	/*for(int i=0;i<1;i++){
-		writeTo(array,pFile);
-		array = dostep(array);
-		showMatrix(array);
-		sleep(1);
-	}*/
-
 	while(1){
+		sleep(1);
+		system("clear");
 		array = dostep(array);
 		showMatrix(array);
-		sleep(1);
-		//system("clear");
 	}
-
-	fclose(pFile);
-
 }
